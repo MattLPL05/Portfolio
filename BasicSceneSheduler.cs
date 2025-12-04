a10865c80e8f874bdb22ce2f5f9f4de78b621cdc
MattLPL05 <94893650+MattLPL05@users.noreply.github.com>
2025-12-04 13:34:30 +0100

    Create BasicSceneSheduler.cs

---
+++ BasicSceneSheduler.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BasicSceneSheduler : MonoBehaviour
+{
+    
+    
+    [Header("If scene is not specific, text is not required")]
+    [SerializeField]
+    private bool IsSpecific;
+    [SerializeField]
+    private bool IsPsychicial;
+    [SerializeField]
+    private string SceneName;
+    [SerializeField]
+    private float Delay; //delay, in seconds
+
+    //[Header]
+    //If scene is not specific, text is not required
+    
+     void OnTriggerEnter(Collider other)
+     {
+        Debug.Log("Box Detected!");
+        if(IsPsychicial == true)
+        {
+            if(IsSpecific == true)
+            {
+            StartCoroutine(SheduleSceneSwitch());
+            }
+            else if (IsSpecific == false)
+            {
+            StartCoroutine(SheduleSceneSwitch());
+            }
+        }
+     }
+
+    public void ChangeScene()
+    {
+        if(IsSpecific == true)
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+        else if (IsSpecific == false)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+
+    private IEnumerator SheduleSceneSwitch()
+    {
+        yield return new WaitForSeconds(Delay);
+        ChangeScene();
+    }
+
+}
